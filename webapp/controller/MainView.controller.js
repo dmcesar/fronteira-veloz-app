27a43b768549804ff152bca5e259b629b6441539
dmour <dmourao.cesar@gmail.com>
2021-09-18 21:14:59 +0100

    package.json updated

--- webapp/controller/MainView.controller.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define(["pt/FRONTEIRAVELOZ/controller/BaseController"], function (Controller) {
-    "use strict";
-
-    return Controller.extend("pt.FRONTEIRAVELOZ.controller.MainView", {});
-});
