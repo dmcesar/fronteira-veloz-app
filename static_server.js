8bcda004f524acf31242288cf78827d0b6d0d5d8
dmour <dmourao.cesar@gmail.com>
2021-09-18 20:48:55 +0100

    Added port argument to ui5 serve script

--- static_server.js
+++
@@ -1,8 +0,0 @@
-proxy_cfg = {
-    // the prefix you use to call your backend functions via the proxy server
-    prefix: "/proxy/",
-    // the host of your backend server
-    host: "fronteira-veloz.pt",
-    // port of your backend server
-    port: "8080"
-  };
